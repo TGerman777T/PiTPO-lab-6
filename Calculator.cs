9779d8be267c8509d8934ae015aff3964f5038ef
German777 <47473290+TGerman777T@users.noreply.github.com>
2024-05-27 21:26:13 +0600

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniTestApp
+{
+    internal class Calculator
+    {
+        public static int CalculateSum(string numbers, char delimiter = ',')
+        {
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+
+            var numArray = numbers.Split(delimiter).Select(n =>
+            {
+                if (!int.TryParse(n, out int num))
+                {
+                    throw new Exception("Input string contains non-numeric characters.");
+                }
+                return num;
+            })
+                                  .Where(n => n <= 10)
+                                  .Take(5)
+                                  .ToArray();
+
+            if (numArray.Any(n => n < 0))
+            {
+                return 0;
+            }
+
+            return numArray.Sum();
+        }
+    }
+}
---
+++ CalculatorTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UniTestApp
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public static void SimpleTest()
+        {
+            var result = Calculator.CalculateSum("3,8,4,2,7");
+            Assert.Equal(24, result);
+        }
+
+        [Fact]
+        public static void TestWith10()
+        {
+            var result = Calculator.CalculateSum("3,8,4,2,12");
+            Assert.Equal(17, result);
+        }
+
+        [Fact]
+        public static void OnlyFirst5Test()
+        {
+            var result = Calculator.CalculateSum("3,8,4,2,7,1,5");
+            Assert.Equal(24, result);
+        }
+
+        [Fact]
+        public static void NegativeTest()
+        {
+            var result = Calculator.CalculateSum("3,8,-4,2,7");
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public static void DelimiterTest()
+        {
+            var result = Calculator.CalculateSum("3|8|4|2|7", '|');
+            Assert.Equal(24, result);
+        }
+
+        [Fact]
+        public static void NonNumeralTest()
+        {
+            Assert.Throws<Exception>(() => Calculator.CalculateSum("3,8,4,a,7"));
+        }
+
+        [Fact]
+        public static void TestWithNothing()
+        {
+            var result = Calculator.CalculateSum("");
+            Assert.Equal(0, result);
+        }
+    }
+}
